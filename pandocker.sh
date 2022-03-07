532b3c87af0702dcc992bc7e293d8b4becebc981
Kristinn Stefánsson <kristinn.stefansson@arionbanki.is>
2022-03-07 07:52:16 +0000

    Update fonts and styles

--- pandocker.sh
+++ pandocker.sh
@@ -1,12 +1,25 @@
 # alias pandocker='docker run --rm -v "$(pwd):/pandoc" -u $(id -u):$(id -g) artifactory.arionbanki.is/docker.io-remote/dalibo/pandocker'
 # docker run --rm --volume "`pwd`:/data" --user `id -u`:`id -g` artifactory.arionbanki.is/docker.io-remote/pandoc/latex:latest --lua-filter=pagebreak.lua  -f markdown+hard_line_breaks -s '310.md' -t pdf -o 310_A.pdf --highlight-style tango
 
+function doc()
+{
+  outext=$1
+  docker run --rm -v "$(pwd):/pandoc" -u $(id -u):$(id -g) kristinn/pandocker:buster-full-latest "Vinnusvæði/Verkþáttur 5/${fullname}.md" -o "Vinnusvæði/Verkþáttur 5/${fullname}.${outext}" \
+    --from markdown --filter pandoc-include --template 'lib/ist.tex' --listings --filter pandoc-codeblock-include --filter pandoc-crossref --filter pandoc-mustache \
+    --citeproc --top-level-division=chapter --pdf-engine=xelatex \
+    --variable 'mainfont:Arial' --variable 'mainfontoptions:Scale=0.94, Extension=.ttf, UprightFont=*, BoldFont=*_Bold, ItalicFont=*_Italic, BoldItalicFont=*_Bold_Italic' \
+    --variable 'sansfont:Arial' --variable 'sansfontoptions:Scale=0.94, Extension=.ttf, UprightFont=*, BoldFont=*_Bold, ItalicFont=*_Italic, BoldItalicFont=*_Bold_Italic' \
+    --variable 'monofont:YaHei Consolas Hybrid' --variable 'monofontoptions:Scale=1'
+}
+
 function ex()
 {
   fullname=$1
   echo "$fullname"
   # artifactory.arionbanki.is/docker.io-remote/dalibo/pandocker
-  docker run --rm -v "$(pwd):/pandoc" -u $(id -u):$(id -g) kristinn/pandocker:buster-full-latest "Vinnusvæði/Verkþáttur 5/${fullname}.md" -o "Vinnusvæði/Verkþáttur 5/${fullname}.pdf" --from markdown --filter pandoc-include --template 'lib/ist.tex' --listings --filter pandoc-codeblock-include --filter pandoc-crossref --filter pandoc-mustache --citeproc --top-level-division=chapter --pdf-engine=xelatex
+
+  doc "pdf"
+  doc "tex"
 }
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 ex "ÍST TS 310_2020 Domestic payments and deposits"

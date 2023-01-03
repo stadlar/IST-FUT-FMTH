06d788c331678fa5e764c25a5fe0991f74a2384b
Kristinn Stefansson <kristinn.stefansson@arionbanki.is>
2023-01-03 15:50:20 +0000

    Rename files from 2022 to 2023

--- pandocker.sh
+++ pandocker.sh
@@ -28,8 +28,8 @@ then
   cp "out/docs/310and313media/AuthFlow1/AuthFlow1.eps" "lib/media/authtflow1.eps"
 fi
 # ex "ÍST WA 316_2022 IOBWS 3.0 Technical Guidelines"
-ex "ÍST TS 313_2022 Foreign payments"
-ex "ÍST TS 310_2022 Domestic payments and deposits"
+ex "ÍST TS 313_2023 Foreign payments"
+ex "ÍST TS 310_2023 Domestic payments and deposits"
 # ex "ÍST TS 311_2022 Debit and credit cards details and statements"
 # ex "ÍST TS 312_2022 Currency Exchange Rates"
 # ex "ÍST TS 314_2022 Digital Documents"

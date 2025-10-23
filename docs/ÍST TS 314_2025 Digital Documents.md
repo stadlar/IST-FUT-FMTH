8a42f416fcede3423a54151b1897d15735b71176
Kristinn Stefánsson <kristinn.stefansson@arionbanki.is>
2025-10-23 13:12:24 +0000

    Update entry into force dates for ÍST TS 314 and TS 315 documents

--- docs/ÍST TS 314_2025 Digital Documents.md
+++ docs/ÍST TS 314_2025 Digital Documents.md
@@ -1,7 +1,7 @@
 ---
 title: "ÍST TS 314:2025"
 author: ICS 35.240, 3.060
-date: "Entry into force 21.03.2025"
+date: "Entry into force 20.11.2025"
 subject: "Icelandic Online Banking Webservices "
 keywords: [IOBWS, ÍST, TS, 314]
 subtitle: ["Tækniforskrift - Rafræn skjöl", "Technical specification - Digital Documents"]
--- pandocker.sh
+++ pandocker.sh
@@ -27,7 +27,7 @@ if [ -f "out/docs/310and313media/AuthFlow1/AuthFlow1.eps" ]
 then 
   cp "out/docs/310and313media/AuthFlow1/AuthFlow1.eps" "lib/media/authtflow1.eps"
 fi
-ex "ÍST TS 316_2025 IOBWS 3.0 Technical Guidelines"
+# ex "ÍST TS 316_2025 IOBWS 3.0 Technical Guidelines"
 # ex "ÍST TS 313_2023 Foreign payments"
 # ex "ÍST TS 310_2023 Domestic payments and deposits"
 # ex "ÍST TS 311_2022 Debit and credit cards details and statements"
